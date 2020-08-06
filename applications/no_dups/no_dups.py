3509e7a9350a764ca0630fe795984cdbe42972d7
Blayze <blayzestone@protonmail.com>
2020-08-06 12:51:33 -0700

    implement 'no_dups' application

--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -1,11 +1,18 @@
 def no_dups(s):
-    # Your code here
+    prevWords = set(())
+    words = s.split(" ")
+    no_duplicates_string = ""
 
+    for w in words:
+        if w not in prevWords:
+            no_duplicates_string += f"{w} "
+
+        prevWords.add(w)
+
+    return str.strip(no_duplicates_string)
 
 
 if __name__ == "__main__":
-    print(no_dups(""))
-    print(no_dups("hello"))
     print(no_dups("hello hello"))
     print(no_dups("cats dogs fish cats dogs"))
     print(no_dups("spam spam spam eggs spam sausage spam spam and spam"))
