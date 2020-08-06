a5907223e9b28fa772ae1b982907d7abde6f41a3
Blayze <blayzestone@protonmail.com>
2020-08-06 08:48:55 -0700

    implement 'word_count' application

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,10 +1,52 @@
 def word_count(s):
-    # Your code here
+    ignored_chars = (
+        '"',
+        ":",
+        ";",
+        ",",
+        ".",
+        "-",
+        "+",
+        "=",
+        "/",
+        "\\",
+        "|",
+        "[",
+        "]",
+        "{",
+        "}",
+        "(",
+        ")",
+        "*",
+        "^",
+        "&",
+    )
+    word_counts = {}
+    formattedString = ""
 
+    for char in s:
+        if char not in ignored_chars:
+            formattedString += char
+
+    formattedString = formattedString.split()
+
+    for word in formattedString:
+        if word:
+            word = word.lower()
+            if word in word_counts:
+                word_counts[word] += 1
+            else:
+                word_counts[word] = 1
+
+    return word_counts
 
 
 if __name__ == "__main__":
-    print(word_count(""))
     print(word_count("Hello"))
     print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
-    print(word_count('This is a test of the emergency broadcast network. This is only a test.'))
+    print(
+        word_count(
+            "This is a test of the emergency broadcast network. This is only a test."
+        )
+    )
+
